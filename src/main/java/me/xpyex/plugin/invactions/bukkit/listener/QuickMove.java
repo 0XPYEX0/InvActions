39157acf9b9ef13a65d4418dcb65aeba8b031d72
XPYEX <XPYEX0@163.com>
2022-12-22 03:47:37 +0800

    修复Bug；令“快速移动”功能会通知其它插件

--- src/main/java/me/xpyex/plugin/invactions/bukkit/listener/QuickMove.java
+++ src/main/java/me/xpyex/plugin/invactions/bukkit/listener/QuickMove.java
@@ -1,15 +1,20 @@
 package me.xpyex.plugin.invactions.bukkit.listener;
 
+import me.xpyex.plugin.invactions.bukkit.InvActions;
 import me.xpyex.plugin.invactions.bukkit.util.InvUtil;
 import me.xpyex.plugin.invactions.bukkit.util.SortUtil;
 import me.xpyex.plugin.xplib.bukkit.util.inventory.ItemUtil;
 import me.xpyex.plugin.xplib.bukkit.util.strings.MsgUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class QuickMove implements Listener {
     @EventHandler
@@ -17,13 +22,16 @@ public class QuickMove implements Listener {
         if (!(event.getWhoClicked() instanceof Player)) {
             return;
         }
+        if (event.getWhoClicked().hasMetadata("InvActions_CallingClick")) {  //正在广播事件给插件处理，自己无需处理
+            return;
+        }
         if (event.getClick() == ClickType.MIDDLE) {  //鼠标中键
             if (event.getCursor() == null) {  //光标拿着的物品
                 return;
             }
-            ItemStack tool = new ItemStack(event.getWhoClicked().getInventory().getItemInMainHand());  //保存手上道具，待会set回去，下面丢出会覆盖主手道具
             ItemStack item = new ItemStack(event.getCursor());
             if (event.getClickedInventory() == null) {  //这就是丢出
+                ItemStack tool = new ItemStack(event.getWhoClicked().getInventory().getItemInMainHand());  //保存手上道具，待会set回去，下面丢出会覆盖主手道具
                 for (ItemStack content : event.getWhoClicked().getInventory().getStorageContents()) {
                     if (content == null) continue;
 
@@ -36,10 +44,17 @@ public class QuickMove implements Listener {
                 }
 
                 if (event.getWhoClicked().getOpenInventory().getTopInventory() != event.getWhoClicked().getInventory()) {
-                    for (ItemStack content : event.getWhoClicked().getOpenInventory().getTopInventory()) {
+                    event.getWhoClicked().setMetadata("InvActions_CallingClick", new FixedMetadataValue(InvActions.getInstance(), true));
+                    for (int i = 0; i < event.getWhoClicked().getOpenInventory().getTopInventory().getStorageContents().length; i++) {
+                        ItemStack content = event.getWhoClicked().getOpenInventory().getTopInventory().getItem(i);
                         if (content == null) continue;
 
                         if (ItemUtil.equals(content, item)) {
+                            InventoryClickEvent clickEvent = new InventoryClickEvent(event.getView(), InventoryType.SlotType.OUTSIDE, i, ClickType.DROP, InventoryAction.DROP_ALL_SLOT);
+                            Bukkit.getPluginManager().callEvent(clickEvent);
+                            if (clickEvent.isCancelled()) {
+                                continue;
+                            }
                             ItemStack copied = new ItemStack(content);
                             event.getWhoClicked().getInventory().setItemInMainHand(copied);
                             content.setAmount(0);
@@ -48,6 +63,12 @@ public class QuickMove implements Listener {
                     }
                 }
 
+                InventoryClickEvent clickEvent = new InventoryClickEvent(event.getView(), InventoryType.SlotType.OUTSIDE, event.getWhoClicked().getInventory().getHeldItemSlot(), ClickType.DROP, InventoryAction.DROP_ALL_SLOT);
+                Bukkit.getPluginManager().callEvent(clickEvent);
+                if (clickEvent.isCancelled()) {
+                    event.getWhoClicked().removeMetadata("InvActions_CallingClick", InvActions.getInstance());
+                    return;
+                }
                 event.getCursor().setAmount(0);
                 event.getWhoClicked().getInventory().setItemInMainHand(item);
                 event.getWhoClicked().dropItem(true);  //丢出光标道具
@@ -55,40 +76,57 @@ public class QuickMove implements Listener {
                 event.getWhoClicked().getInventory().setItemInMainHand(tool);
                 ((Player) event.getWhoClicked()).updateInventory();
                 MsgUtil.sendActionBar((Player) event.getWhoClicked(), "&a已丢出所有相同道具. " + SortUtil.SETTING_HELP);
+                event.getWhoClicked().removeMetadata("InvActions_CallingClick", InvActions.getInstance());
             } else if (event.getClickedInventory() == event.getWhoClicked().getInventory()) {
                 if (event.getWhoClicked().getOpenInventory().getTopInventory() == event.getWhoClicked().getInventory()) {
                     return;
                 }
                 if (InvUtil.isNotMenu(event.getWhoClicked().getOpenInventory().getTopInventory())) {
-                    for (ItemStack content : event.getWhoClicked().getOpenInventory().getTopInventory().getStorageContents()) {
+                    event.getWhoClicked().setMetadata("InvActions_CallingClick", new FixedMetadataValue(InvActions.getInstance(), true));
+                    for (int i = 0; i < event.getWhoClicked().getOpenInventory().getTopInventory().getStorageContents().length; i++) {
+                        ItemStack content = event.getWhoClicked().getOpenInventory().getTopInventory().getItem(i);
                         int firstEmpty = event.getWhoClicked().getInventory().firstEmpty();
                         if (firstEmpty == -1) break;
 
                         if (content == null) continue;
 
                         if (ItemUtil.equals(content, item)) {
+                            InventoryClickEvent clickEvent = new InventoryClickEvent(event.getView(), InventoryType.SlotType.CONTAINER, i, ClickType.SHIFT_LEFT, InventoryAction.MOVE_TO_OTHER_INVENTORY);
+                            Bukkit.getPluginManager().callEvent(clickEvent);  //从容器移动到玩家背包
+                            if (clickEvent.isCancelled()) {
+                                continue;
+                            }
                             ItemStack copied = new ItemStack(content);
                             event.getWhoClicked().getInventory().setItem(firstEmpty, copied);
                             content.setAmount(0);
                         }
                     }
                     MsgUtil.sendActionBar((Player) event.getWhoClicked(), "&a已将所有相同道具移至你背包. " + SortUtil.SETTING_HELP);
+                    event.getWhoClicked().removeMetadata("InvActions_CallingClick", InvActions.getInstance());
                 }
             } else if (event.getClickedInventory() == event.getWhoClicked().getOpenInventory().getTopInventory()) {
                 if (InvUtil.isNotMenu(event.getClickedInventory())) {
-                    for (ItemStack content : event.getWhoClicked().getInventory().getStorageContents()) {
+                    event.getWhoClicked().setMetadata("InvActions_CallingClick", new FixedMetadataValue(InvActions.getInstance(), true));
+                    for (int i = 0; i < event.getWhoClicked().getInventory().getStorageContents().length; i++) {
+                        ItemStack content = event.getWhoClicked().getInventory().getItem(i);
+
                         int firstEmpty = event.getWhoClicked().getOpenInventory().getTopInventory().firstEmpty();
                         if (firstEmpty == -1) break;
 
                         if (content == null) continue;
 
                         if (ItemUtil.equals(content, item)) {
+                            InventoryClickEvent clickEvent = new InventoryClickEvent(event.getView(), InventoryType.SlotType.CONTAINER, i, ClickType.SHIFT_LEFT, InventoryAction.MOVE_TO_OTHER_INVENTORY);
+                            if (clickEvent.isCancelled()) {  //从玩家背包移动到容器
+                                continue;
+                            }
                             ItemStack copied = new ItemStack(content);
                             event.getWhoClicked().getOpenInventory().getTopInventory().setItem(firstEmpty, copied);
                             content.setAmount(0);
                         }
                     }
                     MsgUtil.sendActionBar((Player) event.getWhoClicked(), "&a已将所有相同道具移至容器内. " + SortUtil.SETTING_HELP);
+                    event.getWhoClicked().removeMetadata("InvActions_CallingClick", InvActions.getInstance());
                 }
             }
         }
