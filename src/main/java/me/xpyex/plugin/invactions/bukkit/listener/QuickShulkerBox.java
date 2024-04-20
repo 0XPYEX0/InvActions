62fc185785d924f3e4b67ba0d32e8e3be299424f
XPYEX <XPYEX0@163.com>
2024-04-21 00:08:14 +0800

    加入潜影盒开关音效

--- src/main/java/me/xpyex/plugin/invactions/bukkit/listener/QuickShulkerBox.java
+++ src/main/java/me/xpyex/plugin/invactions/bukkit/listener/QuickShulkerBox.java
@@ -5,11 +5,13 @@ import me.xpyex.plugin.invactions.bukkit.config.InvActionsServerConfig;
 import me.xpyex.plugin.invactions.bukkit.util.SettingsUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -20,11 +22,11 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 
 public class QuickShulkerBox implements Listener {
-
     private static final String METADATA_KEY = "InvActions_Shulker";
 
     @EventHandler
     public void onInvClick(InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player)) return;
         if (InvActionsServerConfig.getConfig().QuickShulkerBox) {
             if (SettingsUtil.getConfig((Player) event.getWhoClicked()).QuickShulkerBox) {
                 if (event.isShiftClick() && event.isRightClick()) {
@@ -32,11 +34,12 @@ public class QuickShulkerBox implements Listener {
                         return;
                     if (event.getCurrentItem() == null) return;
                     ItemMeta meta = event.getCurrentItem().getItemMeta();
-                    if (meta instanceof BlockStateMeta) {
+                    if (meta instanceof BlockStateMeta) {  //此处同时判断 != null
                         BlockState state = ((BlockStateMeta) meta).getBlockState();
                         if (state instanceof ShulkerBox) {
                             event.getWhoClicked().openInventory(((ShulkerBox) state).getInventory());
                             event.getWhoClicked().setMetadata(METADATA_KEY, new FixedMetadataValue(InvActions.getInstance(), event.getCurrentItem()));
+                            ((Player) event.getWhoClicked()).playSound(event.getWhoClicked().getLocation(), Sound.BLOCK_SHULKER_BOX_OPEN, 1f, 1f);
                         }
                     }
                 }
@@ -46,7 +49,7 @@ public class QuickShulkerBox implements Listener {
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
-        if (event.getPlayer().isSneaking()) {
+        if (event.getPlayer().isSneaking() && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
             if (event.getItem() != null && event.getItem().hasItemMeta()) {
                 ItemMeta meta = event.getItem().getItemMeta();
                 if (meta instanceof BlockStateMeta) {
@@ -54,6 +57,7 @@ public class QuickShulkerBox implements Listener {
                     if (state instanceof ShulkerBox) {
                         event.getPlayer().openInventory(((ShulkerBox) state).getInventory());
                         event.getPlayer().setMetadata(METADATA_KEY, new FixedMetadataValue(InvActions.getInstance(), event.getItem()));
+                        event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_SHULKER_BOX_OPEN, 1f, 1f);
                     }
                 }
             }
@@ -82,17 +86,19 @@ public class QuickShulkerBox implements Listener {
 
     @EventHandler
     public void onInvClose(InventoryCloseEvent event) {
+        if (!(event.getPlayer() instanceof Player)) return;
         if (event.getPlayer().hasMetadata(METADATA_KEY)) {
             ItemStack stack = (ItemStack) event.getPlayer().getMetadata(METADATA_KEY).get(0).value();
             if (stack != null) {
                 ItemMeta meta = stack.getItemMeta();
-                if (meta instanceof BlockStateMeta) {
+                if (meta instanceof BlockStateMeta) {  //此处同时判断 != null
                     BlockState state = ((BlockStateMeta) meta).getBlockState();
                     if (state instanceof ShulkerBox) {
                         ((ShulkerBox) state).getInventory().setContents(event.getInventory().getContents());
                         ((BlockStateMeta) meta).setBlockState(state);
                         stack.setItemMeta(meta);
                         event.getPlayer().removeMetadata(METADATA_KEY, InvActions.getInstance());
+                        ((Player) event.getPlayer()).playSound(event.getPlayer().getLocation(), Sound.BLOCK_SHULKER_BOX_CLOSE, 1f, 1f);
                         Bukkit.getScheduler().runTaskLater(InvActions.getInstance(), () -> {
                             ((Player) event.getPlayer()).updateInventory();
                         }, 1L);
