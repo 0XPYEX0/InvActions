da80516e6fdfe35b91d4b6af8ecb30a237c62502
XPYEX <XPYEX0@163.com>
2025-05-30 01:22:32 +0800

    feat: 完善EggCatcher

--- MC12/src/main/java/me/xpyex/plugin/invactions/bukkit/message/module/EggCatcherMessage.java
+++ MC12/src/main/java/me/xpyex/plugin/invactions/bukkit/message/module/EggCatcherMessage.java
@@ -1,4 +1,11 @@
 package me.xpyex.plugin.invactions.bukkit.message.module;
 
+import lombok.Data;
+import me.xpyex.plugin.invactions.bukkit.message.MenuInfo;
+
+@Data
 public class EggCatcherMessage {
+    private String caught = "&a你成功用鸡蛋捕获了一个 &r{0} &a！";
+    private String failed = "&c布嚎！捕捉 &r{0} &c失败了，它跑了！";
+    private MenuInfo menu = MenuInfo.of("&a捕捉生物", "&f使用鸡蛋命中实体", "&f能够将其捕捉进刷怪蛋", "&f可以拾取");
 }
--- MC13/src/main/java/me/xpyex/plugin/invactions/bukkit/module/EggCatcher.java
+++ MC13/src/main/java/me/xpyex/plugin/invactions/bukkit/module/EggCatcher.java
@@ -2,11 +2,15 @@ package me.xpyex.plugin.invactions.bukkit.module;
 
 import java.util.Random;
 import me.xpyex.lib.xplib.bukkit.inventory.ItemUtil;
+import me.xpyex.lib.xplib.bukkit.language.LangUtil;
+import me.xpyex.lib.xplib.bukkit.strings.MsgUtil;
 import me.xpyex.lib.xplib.bukkit.version.VersionUtil;
+import me.xpyex.plugin.invactions.bukkit.InvActions;
 import me.xpyex.plugin.invactions.bukkit.config.InvActionsServerConfig;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.ProjectileHitEvent;
@@ -14,12 +18,14 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 public class EggCatcher extends RootModule {
     private static final Random RANDOM = new Random(System.currentTimeMillis());
 
-    private static void catchEntity(Entity entity) {
+    private static Item catchEntity(Entity entity) {
         Material spawnEggType = Material.getMaterial(entity.getType() + "_SPAWN_EGG");
         if (spawnEggType != null) {
-            entity.getWorld().dropItem(entity.getLocation(), ItemUtil.getItemStack(spawnEggType, entity.getName()));
+            Item item = entity.getWorld().dropItem(entity.getLocation(), ItemUtil.getItemStack(spawnEggType, entity.getName()));
             entity.remove();
+            return item;
         }
+        return null;
     }
 
     @Override
@@ -36,12 +42,20 @@ public class EggCatcher extends RootModule {
                 if (event.getEntity().getShooter() instanceof Player) {
                     Player p = (Player) event.getEntity().getShooter();
                     if (playerEnabled(p)) {
-                        Integer chance = InvActionsServerConfig.getCurrent().getEggCatcher_Chance().get(event.getHitEntity().getType().toString());
-                        if (chance == null) {
-                            catchEntity(event.getHitEntity());
-                        } else if (chance >= 0 && chance <= 100) {
+                        EntityType hitEntityType = event.getHitEntity().getType();
+                        MsgUtil.debugLog(InvActions.getInstance(), "EggCatcher: 玩家 " + p.getName() + " 用鸡蛋命中 " + hitEntityType);
+                        Integer chance = InvActionsServerConfig.getCurrent().getEggCatcher_Chance().get(hitEntityType.toString());
+                        if (chance == null || chance == 100) {
+                            if (catchEntity(event.getHitEntity()) != null) {
+                                MsgUtil.sendActionBar(p, getMessageWithSuffix("caught", LangUtil.getTranslationName(InvActions.getInstance(), hitEntityType)));
+                            }
+                        } else if (chance > 0 && chance < 100) {
                             if (RANDOM.nextInt(100) < chance) {
-                                catchEntity(event.getHitEntity());
+                                if (catchEntity(event.getHitEntity()) != null) {
+                                    MsgUtil.sendActionBar(p, getMessageWithSuffix("caught", LangUtil.getTranslationName(InvActions.getInstance(), hitEntityType)));
+                                }
+                            } else {
+                                MsgUtil.sendActionBar(p, getMessageWithSuffix("failed", LangUtil.getTranslationName(InvActions.getInstance(), hitEntityType)));
                             }
                         }
                     }
